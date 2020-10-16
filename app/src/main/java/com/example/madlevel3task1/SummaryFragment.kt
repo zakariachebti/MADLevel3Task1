22bc08d1e378d8148a42abc620fe71b149854c28
Zakaria Chebti <zakaria@chipta.com>
2020-10-16 21:41:03 +0200

    Step 5

--- app/src/main/java/com/example/madlevel3task1/SummaryFragment.kt
+++ app/src/main/java/com/example/madlevel3task1/SummaryFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_summary.*
 
 const val ARG_GAME_NAME = "arg_game_name"
 const val ARG_GAME_RATING = "arg_game_rating"
@@ -18,4 +20,19 @@ class SummaryFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_summary, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val gameName = arguments?.getString(ARG_GAME_NAME)
+        val gameRating = arguments?.getFloat(ARG_GAME_RATING)
+
+        txt_summary.text = String.format("You rated %s with %.1f stars! Thanks?", gameName, gameRating)
+
+        btn_start_over.setOnClickListener {
+            findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
+        }
+
+    }
+
 }
